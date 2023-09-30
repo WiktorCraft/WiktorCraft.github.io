a71418845f6e501d3223e7fc370ad6e6b01d2517
WiktorCraft <wiktorcraft2137@hotmail.com>
2023-09-30 16:51:15 +0200

    xd
    xd

--- resources/scripts/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { App } from '@/components/App';
-
-// Enable language support.
-import './i18n';
-
-createRoot(document.getElementById('app')!).render(<App />);
